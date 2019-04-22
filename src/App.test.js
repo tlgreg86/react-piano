34df242f7def0c5b60c001d987bcf09477ba796f
Travis Gregory <23178810+tlgreg86@users.noreply.github.com>
2019-04-22 02:50:47 -0600

    Add App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+
+  const makeWrapper = () => shallow(<App />)
+
+  it('should render without crashing', () => {
+    const wrapper = makeWrapper()
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('should update keysLogged in state when handleSubmit is executed', () => {
+    const wrapper = makeWrapper()
+    const instance = wrapper.instance()
+
+    expect(wrapper.state('keysLogged')).toEqual([])
+    instance.handleClick('A')
+    expect(wrapper.state('keysLogged')).toEqual(['A'])
+  });
+
+  it('should update keysPlayed in state and execute the playPiano method', async () => {
+    const wrapper = makeWrapper()
+    const instance = wrapper.instance()
+
+    expect(wrapper.state('keysPlayed')).toEqual([])
+    jest.spyOn(instance, 'playPiano')
+    await instance.handleSubmit(['A','B','C'])
+    expect(wrapper.state('keysPlayed')).toEqual(['A', 'B', 'C'])
+    expect(instance.playPiano).toHaveBeenCalledTimes(1)
+  });
+
+  it.todo('should update keys state when playPiano is executed');
+
+})
