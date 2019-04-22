30a05a4ca50f66f2b078527c892c1abfcbe9b4ac
Travis Gregory <23178810+tlgreg86@users.noreply.github.com>
2019-04-21 23:03:29 -0600

    Add userInputArray to PlayerInput state

---
+++ src/components/PlayerInput/PlayerInput.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class PlayerInput extends Component {
+  state = {
+    userInputArray: [],
+    userInput: '',
+    inputValid: false,
+  }
+
+  handleChange = (value) => {
+    const userInputArray = value.split(',')
+    console.log(userInputArray)
+    this.setState({
+      userInputArray,
+      userInput: value,
+    })
+    this.validateString(value)
+  }
+
+  validateString = (value) => {
+    let inputValid = /^([CcDdEFfGgAaB]{1},)*[CcDdEFfGgAaB]$/.test(value)
+    this.setState({inputValid})
+  }
+
+  render() {
+    const {handleSubmit} = this.props
+    return (
+      <div className='player-input-wrapper'>
+        {
+          this.state.inputValid ? 
+            <div><span className='valid-input'>√</span> Valid Input</div> 
+            : <div><span className='invalid-input'>X</span> Invalid Input</div> 
+        }
+        <div className='player-input'>
+          <textarea 
+            onChange={(e) => this.handleChange(e.target.value)}
+            value={this.state.userInput}
+          />
+          <button
+            className='submit-playlist'
+            disabled={!this.state.inputValid}
+            onClick={() => handleSubmit(this.state.userInputArray)}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
