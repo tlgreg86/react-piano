d02ef6ea92408ca1e82a808c0eea47f051dcb085
Travis Gregory <tgregory@shutterstock.com>
2024-12-02 16:32:05 -0500

    convert  PlayerInput component to a func component and add focus handling

--- src/components/PlayerInput/PlayerInput.js
+++ src/components/PlayerInput/PlayerInput.js
@@ -1,54 +1,60 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
 
-export default class PlayerInput extends Component {
-  state = {
-    userInputArray: [],
-    userInput: '',
-    inputValid: false,
-  }
+const PlayerInput = ({ handleSubmit, setIsInputFocused }) => {
+  const [userInputArray, setUserInputArray] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [inputValid, setInputValid] = useState(false);
 
-  handleChange = (value) => {
-    const userInputArray = value.split(',')
-    this.setState({
-      userInputArray,
-      userInput: value,
-    })
-    this.validateString(value)
-  }
+  const validateString = useCallback((value) => {
+    return /^([CcDdEFfGgAaB]{1},)*[CcDdEFfGgAaB]$/.test(value);
+  }, []);
 
-  validateString = (value) => {
-    let inputValid = /^([CcDdEFfGgAaB]{1},)*[CcDdEFfGgAaB]$/.test(value)
-    this.setState({inputValid})
-  }
+  const handleChange = useCallback((value) => {
+    const newInputArray = value.split(',');
+    setUserInputArray(newInputArray);
+    setUserInput(value);
+    setInputValid(validateString(value));
+  }, [validateString]);
 
-  render() {
-    const {handleSubmit} = this.props
-    return (
-      <div className='player-input-wrapper'>
-        {
-          this.state.inputValid ? 
-            <div><span className='valid-input'>√</span> Valid Input</div> 
-            : <div><span className='invalid-input'>X</span> Invalid Input</div> 
-        }
-        <div className='player-input'>
-          <textarea 
-            onChange={(e) => this.handleChange(e.target.value)}
-            value={this.state.userInput}
-          />
-          <button
-            className='submit-playlist'
-            disabled={!this.state.inputValid}
-            onClick={() => handleSubmit(this.state.userInputArray)}
-          >
-            Submit
-          </button>
-        </div>
+  const handleFocus = useCallback(() => {
+    setIsInputFocused(true);
+  }, [setIsInputFocused]);
+
+  const handleBlur = useCallback(() => {
+    setIsInputFocused(false);
+  }, [setIsInputFocused]);
+
+  return (
+    <div className='player-input-wrapper'>
+      {inputValid ? (
+        <div><span className='valid-input'>√</span> Valid Input</div>
+      ) : (
+        <div><span className='invalid-input'>X</span> Invalid Input</div>
+      )}
+      <div className='player-input'>
+        <textarea
+          onChange={(e) => handleChange(e.target.value)}
+          value={userInput}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          placeholder="Enter notes separated by commas (e.g., C,D,E)"
+        />
+        <button
+          className='submit-playlist'
+          disabled={!inputValid}
+          onClick={() => handleSubmit(userInputArray)}
+        >
+          Submit
+        </button>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 PlayerInput.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
-}
+  setIsInputFocused: PropTypes.func.isRequired
+};
+
+export default PlayerInput;
