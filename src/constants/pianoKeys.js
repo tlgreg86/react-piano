a18b53838f279a3c943a7bd88676481b6e403168
Travis Gregory <tgregory@shutterstock.com>
2024-12-02 15:26:27 -0500

    create pianoKeys constant

---
+++ src/constants/pianoKeys.js
@@ -0,0 +1,14 @@
+export const PIANO_KEYS = [
+  { keyLabel: 'C', type: 'major', isActive: false },
+  { keyLabel: 'c', type: 'minor', isActive: false },
+  { keyLabel: 'D', type: 'major', isActive: false },
+  { keyLabel: 'd', type: 'minor', isActive: false },
+  { keyLabel: 'E', type: 'major', isActive: false },
+  { keyLabel: 'F', type: 'major', isActive: false },
+  { keyLabel: 'f', type: 'minor', isActive: false },
+  { keyLabel: 'G', type: 'major', isActive: false },
+  { keyLabel: 'g', type: 'minor', isActive: false },
+  { keyLabel: 'A', type: 'major', isActive: false },
+  { keyLabel: 'a', type: 'minor', isActive: false },
+  { keyLabel: 'B', type: 'major', isActive: false },
+];
